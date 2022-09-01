2f2d583af9243a02f1d7d8f982114166628e262e
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2022-09-01 12:58:05 -0300

    update task.js

--- src/utils/task.js
+++ src/utils/task.js
@@ -3,7 +3,6 @@ let run = true;
 let task = (consult) => {
     return new Promise((resolve, reject) => {
         if (run) {
-            console.log("cargando")
             setTimeout(() => {
                 resolve(consult)
             }, 2000);
