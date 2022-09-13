834d4ffe1e1616e649c9fdadca230aa37cf8941a
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2022-09-13 12:48:05 -0300

    debuggin bugs

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,15 +1,15 @@
 
 
-function ItemConunt({ item, initial, onAdd, rateHandlerMinus, rateHandlerPlus, cart, rate}) {
+function ItemConunt({ item, initial, onAdd, qtyHandlerMinus, qtyHandlerPlus, cart, qty}) {
     return (
         <div className="itemCountContainer">
             <span className="stockText">{item.stock > 0 ? `Quedan ${item.stock} en stock` : `No quedan unidades en stock`}</span>
             <div className="countBox">
-                <button className="btnMinus" onClick={rateHandlerMinus} disabled={rate === initial}><i className="fa-solid fa-minus"></i></button>
-                <p className="countNumber">{rate}</p>
-                <button className="btnPlus" onClick={rateHandlerPlus} disabled={cart >= item.stock || item.stock === 0}><i className="fa-solid fa-plus"></i></button>
+                <button className="btnMinus" onClick={qtyHandlerMinus} disabled={qty === initial}><i className="fa-solid fa-minus"></i></button>
+                <p className="countNumber">{qty}</p>
+                <button className="btnPlus" onClick={qtyHandlerPlus} disabled={cart >= item.stock || item.stock === 0}><i className="fa-solid fa-plus"></i></button>
             </div>
-            <button className="btnAddToCart" onClick={onAdd} disabled={cart >= item.stock || rate === 0}>Agregar al carrito</button>
+            <button className="btnAddToCart" onClick={onAdd} disabled={cart >= item.stock || qty === 0}>Agregar al carrito</button>
         </div>
     )
 }
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,17 +2,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import ItemConunt from "./ItemCount";
 function ItemDetail({ item }) {
-    const initialRate = 0;
-    const [rate, setRate] = useState(initialRate);
-    const rateHandlerPlus = () => {
-        rate < item.stock ? setRate(rate + 1) : setRate(rate)
+    const initialQty = 0;
+    const [qty, setQty] = useState(initialQty);
+    const qtyHandlerPlus = () => {
+        qty < item.stock ? setQty(qty + 1) : setQty(qty)
     }
-    const rateHandlerMinus = () => {
-        rate > 1 ? setRate(rate - 1) : setRate(rate)
+    const qtyHandlerMinus = () => {
+        qty > 1 ? setQty(qty - 1) : setQty(qty)
     }
     let [cart, setCart] = useState(0)
     const onAdd = () => {
-        cart < item.stock ? cart = cart + rate : setCart(0)
+        cart < item.stock ? cart = cart + qty : setCart(0)
         cart === 1 ? console.log(`Se agrego ${cart} objeto al carrito`) : console.log(`Se agregaron ${cart} objetos al carrito`)
         setCart(cart)
     }
@@ -25,10 +25,10 @@ function ItemDetail({ item }) {
                 </div>
                 <div className="itemDetailcard">
                     <p className="itemDetailDescription">{item.desciption}</p>
-                    <p className="itemDetailPrice">${item.price}</p>
                     {
                         cart < 1
-                            ? <ItemConunt stock={item.stock} item={item} initial={initialRate} onAdd={onAdd} cart={cart} rate={rate} rateHandlerMinus={rateHandlerMinus} rateHandlerPlus={rateHandlerPlus} />
+                            ? <><p className="itemDetailPrice">${item.price}</p>
+                                <ItemConunt stock={item.stock} item={item} initial={initialQty} onAdd={onAdd} cart={cart} qty={qty} qtyHandlerMinus={qtyHandlerMinus} qtyHandlerPlus={qtyHandlerPlus} /></>
                             : <Link to='/cart' className="btnAddToCart"><p className="btnGoToCart">Go to cart</p></Link>
                     }
                 </div>
